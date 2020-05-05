f845167782ee5ce078d7be46ed3d8dd489f5cc7e
aapogoretskiy <aapogoretskiy@gmail.com>
2020-05-05 22:57:55 +0300

    Выполнена инкапусляция

--- LAB/src/main/java/Application.java
+++ LAB/src/main/java/Application.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Application {
     static final String DB_URL = "jdbc:postgresql://localhost:5432/products";
     static final String USER = "postgres";
-    static final String PASS = "1234";
+    static final String PASS = "Raf226raf";
     static DBConnect dbConnect = new DBConnect();
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
--- LAB/src/main/java/connection/DbConnect.java
+++ LAB/src/main/java/connection/DbConnect.java
@@ -1,13 +1,22 @@
 package connection;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Connection;
 import java.sql.SQLException;
 
-public class DbConnect {
-    public  Connection getConnect(String dbUrl, String password, String userName) throws ClassNotFoundException, SQLException {
-            Class.forName("org.postgresql.Driver");
-            System.out.println("PostgreSQL JDBC Driver successfully connected");
-            return DriverManager.getConnection(dbUrl, userName, password);
+
+public class DBConnect {
+
+
+        public Connection getConnect(String dburl, String password, String username) throws ClassNotFoundException, SQLException {
+                Class.forName("org.postgresql.Driver");
+                System.out.println("PostgreSQL JDBC Drive successfully connected");
+            return DriverManager.getConnection(dburl, username, password);
+
+
+        }
+
+
     }
-}
+
+
